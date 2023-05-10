28996072991789b4e98b3fb26e79211a1876d571
Emir Buckun <emirbuckun@hotmail.com>
2023-05-10 09:48:14 +0300

    Database configurations added

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,20 @@
+import mongoose from "mongoose";
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB is connected!");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 app.listen(3001, () => console.log("Server Started!"));
