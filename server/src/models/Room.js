91107129a35041034b8e721505ba76b078c4d47d
HaydarTaha <tahatunc163@gmail.com>
2023-05-11 11:06:58 +0300

    Location, Review and Room models Added

---
+++ server/src/models/Room.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const RoomSchema = new mongoose.Schema({
+  RoomID: { type: mongoose.Schema.Types.ObjectId, required: true },
+  HotelID: { type: mongoose.Schema.Types.ObjectId, required: true },
+  RoomType: { type: mongoose.Schema.Types.ObjectId, required: true },
+  Price: { type: Number, required: true },
+  Count: { type: Number, required: true },
+  PersonCapacity: { type: Number, required: true },
+  TotalBed: { type: Number, required: true },
+  TotalBath: { type: Number, required: true },
+  CreateDate: { type: Date, default: Date.now() },
+  UpdateDate: { type: Date, required: false },
+  HasTV: { type: Boolean, required: true },
+  HasInternet: { type: Boolean, required: true },
+  HasAirCondition: { type: Boolean, required: true },
+  HasHeating: { type: Boolean, required: true },
+});
+
+export const RoomModel = mongoose.model("Room", RoomSchema);
