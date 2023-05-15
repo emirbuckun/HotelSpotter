5cb6eb499454740b63adce1d6eed62c43342639a
Emir Buckun <emirbuckun@hotmail.com>
2023-05-15 07:28:54 +0300

    Login page warnings fixed

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -21,31 +21,28 @@ function Login() {
                       <div className="d-flex flex-row align-items-center mb-4">
                         <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
                         <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="email"
-                            id="form3Example3c"
-                            className="form-control"
-                          />
-                          <label className="form-label" for="form3Example3c">
+                          <label className="form-label" htmlFor="mail">
                             Your Email
                           </label>
+                          <input
+                            type="email"
+                            id="mail"
+                            className="form-control"
+                          />
                         </div>
                       </div>
 
                       <div className="d-flex flex-row align-items-center mb-4">
                         <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
                         <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="password"
-                            id="form3Example4c"
-                            className="form-control"
-                          />
-                          <label
-                            className="form-label"
-                            htmlFor="form3Example4c"
-                          >
+                          <label className="form-label" htmlFor="password">
                             Password
                           </label>
+                          <input
+                            type="password"
+                            id="password"
+                            className="form-control"
+                          />
                         </div>
                       </div>
 
@@ -54,11 +51,11 @@ function Login() {
                           className="form-check-input me-2"
                           type="checkbox"
                           value=""
-                          id="form2Example3c"
+                          id="rememberMe"
                         />
                         <label
                           className="form-check-label"
-                          htmlFor="form2Example3"
+                          htmlFor="rememberMe"
                         >
                           Remember Me
                         </label>
@@ -78,7 +75,7 @@ function Login() {
                     <img
                       src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2020/07/14/11/holidays-2020-should-you-stay-or-go.jpg?width=1200&auto=webp&quality=75"
                       className="img-fluid"
-                      alt="Sampleimage"
+                      alt="Sample Image"
                       style={{ borderRadius: "25px" }}
                     />
                   </div>
