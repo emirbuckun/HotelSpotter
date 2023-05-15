7af596493d008093629f619a036389dfea431088
Emir Buckun <emirbuckun@hotmail.com>
2023-05-15 11:19:52 +0300

    Default path arranged

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route path="/payment" element={<Payment />} />
 
         <Route
-          path="/home"
+          path="/"
           exact
           element={
             <React.Fragment>
