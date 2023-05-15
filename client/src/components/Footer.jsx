18371033cb228c2e3326133c9bb293cfe9d13513
Emir Buckun <emirbuckun@hotmail.com>
2023-05-15 07:47:50 +0300

    Class attribute renamed as className

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,40 +3,40 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <div class="container">
-        <footer class="py-3 my-4">
-          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body">
+      <div className="container">
+        <footer className="py-3 my-4">
+          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+            <li className="nav-item">
+              <a href="#" className="nav-link px-2 text-body">
                 Home
               </a>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body">
+            <li className="nav-item">
+              <a href="#" className="nav-link px-2 text-body">
                 Features
               </a>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body">
+            <li className="nav-item">
+              <a href="#" className="nav-link px-2 text-body">
                 Pricing
               </a>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body">
+            <li className="nav-item">
+              <a href="#" className="nav-link px-2 text-body">
                 FAQs
               </a>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body">
+            <li className="nav-item">
+              <a href="#" className="nav-link px-2 text-body">
                 About
               </a>
             </li>
           </ul>
-          <p class="text-center text-body">&copy; 2023 HotelSpotter</p>
+          <p className="text-center text-body">&copy; 2023 HotelSpotter</p>
         </footer>
       </div>
 
-      <div class="b-example-divider"></div>
+      <div className="b-example-divider"></div>
     </div>
   );
 };
