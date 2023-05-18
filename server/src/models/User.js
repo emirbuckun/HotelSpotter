6d3f67002c8ac39538bdea66a290ab8fdcc7c05c
Emir Buckun <emirbuckun@hotmail.com>
2023-05-18 08:18:47 +0300

    Modify user model property names

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  Username: { type: String, required: true, unique: true },
-  FirstName: { type: String, required: true },
-  LastName: { type: String, required: true },
-  Mail: { type: String, required: true },
-  PhoneNumber: { type: String, required: true },
-  Password: { type: String, required: true },
-  CreateDate: { type: Date, default: Date.now() },
-  UpdateDate: { type: Date, required: false },
+  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  mail: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  password: { type: String, required: true },
+  createDate: { type: Date, default: Date.now() },
+  updateDate: { type: Date, required: false },
 });
 
-export const UserModel = mongoose.model("user", UserSchema);
+export const UserModel = mongoose.model("users", UserSchema);
