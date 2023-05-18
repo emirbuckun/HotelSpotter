fa8c94bfa2c6241a2ab3e706ef213155a5a5408c
Emir Buckun <emirbuckun@hotmail.com>
2023-05-18 09:30:40 +0300

    Login method added

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { UserModel } from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -34,6 +35,24 @@ router.post("/register", async (req, res) => {
   await newUser.save();
 
   res.json({ message: "User registered succesfully!" });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.json({ message: "User doesn't exist!" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.json({ message: "Password is incorrect!" });
+  }
+
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.json({ token, userID: user._id });
 });
 
 export { router as userRouter };
