2003f5dda63f4f9e93a86417e9f47b924db5d45e
Emir Buckun <emirbuckun@hotmail.com>
2023-05-18 07:45:53 +0300

    Register method added

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { UserModel } from "../models/User.js";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
@@ -11,3 +12,28 @@ router.get("/getUsers", async (req, res) => {
     res.json(error);
   }
 });
+
+router.post("/register", async (req, res) => {
+  const { username, firstName, lastName, mail, phoneNumber, password } =
+    req.body;
+  const user = await UserModel.findOne({ Username: username });
+
+  if (user) {
+    return res.json({ message: "User already exists!" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new UserModel({
+    username,
+    firstName,
+    lastName,
+    mail,
+    phoneNumber,
+    password: hashedPassword,
+  });
+  await newUser.save();
+
+  res.json({ message: "User registered succesfully!" });
+});
+
+export { router as userRouter };
