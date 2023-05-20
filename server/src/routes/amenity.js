1455d7719df1c08753487ade374f1b50c669aa01
Emir Buckun <emirbuckun@hotmail.com>
2023-05-20 07:14:22 +0300

    Amenity CRUD methods added

--- server/src/routes/amenity.js
+++ server/src/routes/amenity.js
@@ -3,7 +3,7 @@ import { AmenityModel } from "../models/Amenity.js";
 
 const router = express.Router();
 
-router.get("/getAmenities", async (req, res) => {
+router.get("/get", async (req, res) => {
   try {
     const response = await AmenityModel.find({});
     res.json(response);
@@ -11,3 +11,50 @@ router.get("/getAmenities", async (req, res) => {
     res.json(error);
   }
 });
+
+router.get("/get/:id", async (req, res) => {
+  try {
+    const response = await AmenityModel.findById(req.params.id);
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.post("/insert", async (req, res) => {
+  try {
+    const amenity = new AmenityModel(req.body);
+    const response = await amenity.save();
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.put("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const hotelID = req.body.hotelID;
+  const amenity = req.body.amenity;
+
+  try {
+    const response = await AmenityModel.findByIdAndUpdate(
+      id,
+      { hotelID: hotelID, amenity: amenity },
+      { new: true }
+    );
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const response = await AmenityModel.findByIdAndDelete(req.params.id);
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+export { router as amenityRouter };
