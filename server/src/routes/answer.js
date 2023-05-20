ce7f8777ee4875d39c70252fd0c07c13d28af2e9
Emir Buckun <emirbuckun@hotmail.com>
2023-05-20 08:31:41 +0300

    Answer model CRUD methods added

--- server/src/routes/answer.js
+++ server/src/routes/answer.js
@@ -3,7 +3,7 @@ import { AnswerModel } from "../models/Answer.js";
 
 const router = express.Router();
 
-router.get("/getAnswers", async (req, res) => {
+router.get("/get", async (req, res) => {
   try {
     const response = await AnswerModel.find({});
     res.json(response);
@@ -11,3 +11,45 @@ router.get("/getAnswers", async (req, res) => {
     res.json(error);
   }
 });
+
+router.get("/get/:id", async (req, res) => {
+  try {
+    const response = await AnswerModel.findById(req.params.id);
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.post("/insert", async (req, res) => {
+  try {
+    const amenity = new AnswerModel(req.body);
+    const response = await amenity.save();
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.put("/update/:id", async (req, res) => {
+  try {
+    const response = await AnswerModel.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const response = await AnswerModel.findByIdAndDelete(req.params.id);
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+export { router as answerRouter };
