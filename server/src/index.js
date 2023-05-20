3f7402550f004ca79b10abfb954025ae7b9ff964
Emir Buckun <emirbuckun@hotmail.com>
2023-05-20 07:29:26 +0300

    All routes added

--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,19 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import { userRouter } from "./routes/user.js";
 import { amenityRouter } from "./routes/amenity.js";
+import { answerRouter } from "./routes/amenity.js";
+import { hotelRouter } from "./routes/amenity.js";
+import { locationRouter } from "./routes/amenity.js";
+import { logRouter } from "./routes/amenity.js";
+import { ownershipRouter } from "./routes/amenity.js";
+import { pictureRouter } from "./routes/amenity.js";
+import { questionRouter } from "./routes/amenity.js";
+import { reservationRouter } from "./routes/amenity.js";
+import { reviewRouter } from "./routes/amenity.js";
+import { roomRouter } from "./routes/amenity.js";
+import { userRouter } from "./routes/user.js";
+import { userRoleRouter } from "./routes/amenity.js";
 
 const app = express();
 
@@ -13,8 +24,19 @@ app.use(cors());
 
 dotenv.config();
 
-app.use("/user", userRouter);
 app.use("/amenity", amenityRouter);
+app.use("/answer", answerRouter);
+app.use("/hotel", hotelRouter);
+app.use("/location", locationRouter);
+app.use("/log", logRouter);
+app.use("/ownership", ownershipRouter);
+app.use("/picture", pictureRouter);
+app.use("/question", questionRouter);
+app.use("/reservation", reservationRouter);
+app.use("/review", reviewRouter);
+app.use("/room", roomRouter);
+app.use("/user", userRouter);
+app.use("/userRole", userRoleRouter);
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
