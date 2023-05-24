e723f130029b91aa3b57fe019a217428b839b35d
Emir Buckun <emirbuckun@hotmail.com>
2023-05-24 13:46:30 +0300

    App structure simplified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Register from "/src/components/Register";
 import Login from "/src/components/Login";
 import Header from "/src/components/Header";
-import HotelListing from "/src/components/HotelListing";
+import HotelList from "/src/components/HotelList";
 import Footer from "/src/components/Footer";
 import Payment from "/src/components/Payment";
 import SearchBar from "/src/components/SearchBar";
@@ -15,72 +15,19 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-
         <Route path="/register" element={<Register />} />
-
         <Route path="/payment" element={<Payment />} />
-
         <Route path="/userprofile" element={<UserProfile />} />
-
         <Route
           path="/"
           exact
           element={
-            <React.Fragment>
-              <div>
-                <div className="container">
-                  <Header />
-                </div>
-
-                <div className="container">
-                  <SearchBar />
-                </div>
-
-                <div className="container text-center mb-4">
-                  <div className="row">
-                    <div className="col">
-                      <HotelListing />
-                    </div>
-                    <div className="col">
-                      <HotelListing />
-                    </div>
-                    <div className="col">
-                      <HotelListing />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="container text-center mb-4">
-                  <div className="row">
-                    <div className="col">
-                      <HotelListing />
-                    </div>
-                    <div className="col">
-                      <HotelListing />
-                    </div>
-                    <div className="col">
-                      <HotelListing />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="container text-center mb-4">
-                  <div className="row">
-                    <div className="col">
-                      <HotelListing />
-                    </div>
-                    <div className="col">
-                      <HotelListing />
-                    </div>
-                    <div className="col">
-                      <HotelListing />
-                    </div>
-                  </div>
-                </div>
-
-                <Footer />
-              </div>
-            </React.Fragment>
+            <>
+              <Header />
+              <SearchBar />
+              <HotelList />
+              <Footer />
+            </>
           }
         />
       </Routes>
