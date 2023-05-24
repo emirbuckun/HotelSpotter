d63bf1b25fe016dc38b4ae7cee6ac02003d5281e
HaydarTaha <tahatunc163@gmail.com>
2023-05-24 21:01:07 +0300

    Added map feature

--- client/src/pages/HotelDetails.jsx
+++ client/src/pages/HotelDetails.jsx
@@ -1,12 +1,14 @@
 import Layout from "../layout/Layout";
 import HotelDetailsPhoto from "../components/HotelDetailsPhoto";
 import HotelDetailsInformations from "../components/HotelDetailsInformations";
+import MapComponent from "../components/MapComponent";
 
 const HotelDetails = () => {
   return (
     <Layout>
       <HotelDetailsPhoto />
       <HotelDetailsInformations />
+      <MapComponent />
     </Layout>
   );
 };
