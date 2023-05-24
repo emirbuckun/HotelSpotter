0d30d0216f2b3b9af2758a4ecc3837c9c3b30bf8
Emir Buckun <emirbuckun@hotmail.com>
2023-05-24 15:07:50 +0300

    New pages created and added to app jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Layout from "../layout/Layout";
+import SearchBar from "../components/SearchBar";
+import HotelList from "../components/HotelList";
+
+const Home = () => {
+  return (
+    <Layout>
+      <SearchBar />
+      <HotelList />
+    </Layout>
+  );
+};
+
+export default Home;
