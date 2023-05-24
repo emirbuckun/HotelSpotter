88f132c4ec09dc0de7d62e843df9af4579d8bd8d
Emir Buckun <emirbuckun@hotmail.com>
2023-05-24 19:35:23 +0300

    Hotel details added as a page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "/src/pages/Login";
 import Register from "/src/pages/Register";
 import Payment from "/src/pages/Payment";
 import UserProfile from "/src/pages/UserProfile";
+import HotelDetails from "/src/pages/HotelDetails";
 import HotelDetailsPhoto from "/src/components/HotelDetailsPhoto";
 import HotelDetailsInformations from "/src/components/HotelDetailsInformations";
 
@@ -18,30 +19,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/userprofile" element={<UserProfile />} />
+        <Route path="/hoteldetails" element={<HotelDetails />} />
         <Route path="/map" element={<HotelDetailsInformations />} />
-
-        {/* <Route
-          path="/hoteldetails"
-          exact
-          element={
-            <React.Fragment>
-              <div className="container mb-4 mx-4">
-                <Header />
-              </div>
-              <div className="col">
-                <div className="row">
-                  <HotelDetailsPhoto />
-                </div>
-                <div className="row">
-                  <HotelDetailsInformations />
-                </div>
-              </div>
-              <div className="container mb-4 mx-4">
-                <Footer />
-              </div>
-            </React.Fragment>
-          }
-        /> */}
       </Routes>
     </Router>
   );
