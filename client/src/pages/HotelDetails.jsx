e4fbb50d078bd7577a7905e4d6fd3b42b54bc45e
HaydarTaha <tahatunc163@gmail.com>
2023-05-25 18:08:31 +0300

    Solve Problem

--- client/src/pages/HotelDetails.jsx
+++ client/src/pages/HotelDetails.jsx
@@ -2,14 +2,12 @@ import Layout from "../layout/Layout";
 import HotelDetailsPhoto from "../components/HotelDetailsPhoto";
 import HotelDetailsInformations from "../components/HotelDetailsInformations";
 import MapComponent from "../components/MapComponent";
-import HotelDetailsAddComment from "../components/HotelDetailsAddComment";
 
 const HotelDetails = () => {
   return (
     <Layout>
       <HotelDetailsPhoto />
       <HotelDetailsInformations />
-      <HotelDetailsAddComment />
       <MapComponent />
     </Layout>
   );
