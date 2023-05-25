4566103d1f656a4d09c2e5e926ca783e0b25b2c4
Emir Buckun <emirbuckun@hotmail.com>
2023-05-25 15:40:37 +0300

    Payment page modified, regex expressions added for each input.

--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -1,28 +1,25 @@
 import React, { useState } from "react";
 
 function Payment() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [ccName, setCcName] = useState("");
-  const [ccNumber, setCcNumber] = useState("");
-  const [ccExpiration, setCcExpiration] = useState("");
-  const [ccCvv, setCcCvv] = useState("");
+  const [cardName, setCardName] = useState("");
+  const [cardNum, setCardNum] = useState("");
+  const [cardExp, setCardExp] = useState("");
+  const [cvv, setCvv] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
     // Ödeme işlemlerini burada gerçekleştirin veya sunucuya gönderin
   };
 
   return (
     <div className="container">
       <div className="py-5 text-center">
-        <h2>Payment form</h2>
+        <h2>Payment</h2>
       </div>
 
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <form className="needs-validation" noValidate onSubmit={handleSubmit}>
+          <form className="needs-validation" onSubmit={handleSubmit}>
             <ul className="list-group mb-3">
               <li className="list-group-item d-flex justify-content-between">
                 <span>Total price(USD)</span>
@@ -33,100 +30,80 @@ function Payment() {
             <hr className="mb-4" />
 
             <div className="mb-3">
-              <label htmlFor="email">Email</label>
+              <label htmlFor="cardName">Name on the card</label>
               <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="example@example.com"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-              />
-              <div className="invalid-feedback">
-                Please enter a valid email address for shipping updates.
-              </div>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="cc-name">Name on card</label>
-              <input
+                size="25"
                 type="text"
+                name="name"
+                id="cardName"
+                value={cardName}
+                placeholder="Name"
                 className="form-control"
-                id="cc-name"
-                placeholder=""
-                value={ccName}
-                onChange={(event) => setCcName(event.target.value)}
+                onChange={(e) => setCardName(e.target.value)}
                 required
               />
+            </div>
 
-              <div className="invalid-feedback">Name on card is required.</div>
-            </div>
             <div className="mb-3">
-              <label htmlFor="cc-number">Credit card number</label>
+              <label htmlFor="cardNum">Card Number</label>
               <input
                 type="text"
+                id="cardNum"
+                minLength="19"
+                maxLength="19"
+                value={cardNum}
                 className="form-control"
-                id="cc-number"
-                placeholder=""
-                value={ccNumber}
-                onChange={(event) => setCcNumber(event.target.value)}
+                placeholder="0000 0000 0000 0000"
+                pattern="^(?=(\D*\d){16}\D*$)[\d ]*$"
+                onChange={(e) => setCardNum(e.target.value)}
                 required
               />
-              <div className="invalid-feedback">
-                Credit card number is required.
-              </div>
             </div>
+
             <div className="row">
               <div className="col-md-6 mb-3">
-                <label htmlFor="cc-expiration">Expiration Date</label>
+                <label htmlFor="cardExp">Expiration Date</label>
                 <input
                   type="text"
+                  id="cardExp"
+                  minLength="5"
+                  maxLength="5"
+                  value={cardExp}
+                  placeholder="MM/YY"
                   className="form-control"
-                  id="cc-expiration"
-                  placeholder=""
-                  value={ccExpiration}
-                  onChange={(event) => setCcExpiration(event.target.value)}
+                  pattern="^(0[1-9]|[12][0-9]|3[01])[\/][0-9]{2}$"
+                  onChange={(e) => setCardExp(e.target.value)}
                   required
                 />
-                <div className="invalid-feedback">
-                  Expiration date required.
-                </div>
               </div>
+
               <div className="col-md-6 mb-3">
-                <label htmlFor="cc-cvv">CVV</label>
+                <label htmlFor="cvv">CVV</label>
                 <input
                   type="text"
                   className="form-control"
-                  id="cc-cvv"
+                  id="cvv"
                   placeholder=""
-                  value={ccCvv}
-                  onChange={(event) => setCcCvv(event.target.value)}
+                  value={cvv}
+                  minLength="3"
+                  maxLength="3"
+                  pattern="\d{3}"
+                  onChange={(e) => setCvv(e.target.value)}
                   required
                 />
-                <div className="invalid-feedback">Security code required.</div>
               </div>
             </div>
+
             <hr className="mb-4" />
 
             <div className="py-2 text-center">
-              <button
-                className="btn btn-secondary btn-lg btn-block "
-                type="submit"
-              >
-                Continue to Payment
+              <button className="btn btn-primary btn-lg" type="submit">
+                Pay
               </button>
             </div>
           </form>
         </div>
       </div>
-
-      <footer className="my-5 pt-5 text-muted text-center text-small">
-        <p className="mb-1">&copy; 2023-HotelSpotter</p>
-        <ul className="list-inline">
-          <li className="list-inline-item">
-            <a href="#">Privacy</a>
-          </li>
-        </ul>
-      </footer>
     </div>
   );
 }
