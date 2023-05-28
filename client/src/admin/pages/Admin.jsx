a5ec8ce7a30b7e04e75bb8ac15838cf3a1834d20
Emir Buckun <emirbuckun@hotmail.com>
2023-05-28 20:54:09 +0300

    Admin panel page created

---
+++ client/src/admin/pages/Admin.jsx
@@ -0,0 +1,12 @@
+import Layout from "../../layout/Layout";
+import AdminPanel from "../components/Admin";
+
+const Admin = () => {
+  return (
+    <Layout>
+      <AdminPanel />
+    </Layout>
+  );
+};
+
+export default Admin;
