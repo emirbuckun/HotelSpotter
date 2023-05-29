93ba24f4ddd8c9dec25fc25c71fab64f3f6f0531
Emir Buckun <emirbuckun@hotmail.com>
2023-05-29 12:13:53 +0300

    Header moved to top of the table

--- client/src/components/Admin/AdminPanel.jsx
+++ client/src/components/Admin/AdminPanel.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "/src/components/Sidebar/Sidebar";
+import Sidebar from "/src/components/admin/Sidebar";
 import Table from "/src/components/admin/Table";
 import { useParams } from "react-router-dom";
 
@@ -8,12 +8,12 @@ const AdminPanel = () => {
   const pageName = name.charAt(0).toUpperCase() + name.slice(1);
   return (
     <>
-      <h3 className="text-center">Admin Panel - {pageName}</h3>
       <div className="row">
         <div className="col-md-2">
           <Sidebar />
         </div>
         <div className="col-md-10">
+          <h3 className="text-center">Admin Panel - {pageName}</h3>
           <Table name={name} />
         </div>
       </div>
