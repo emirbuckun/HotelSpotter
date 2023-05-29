d8261c1bc48a688f4015507ff8e334ac94caabf6
Emir Buckun <emirbuckun@hotmail.com>
2023-05-29 11:49:23 +0300

    Generic table component created

--- client/src/components/Admin/Table.jsx
+++ client/src/components/Admin/Table.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useFetch from "/src/hooks/useFetch";
 
-const Table = () => {
-  return <div>Table</div>;
+const Table = (name) => {
+  const pageName = name.name;
+  const { data, loading, error } = useFetch(serverURL + "/" + pageName);
+
+  return (
+    <>
+      <div className="table"></div>
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            {data.length > 0 &&
+              Object.keys(data[0]).map((key, index) => (
+                <th key={index}>{key}</th>
+              ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.length > 0 &&
+            data.map((data, index) => (
+              <tr key={index}>
+                {Object.values(data).map((value, index) => (
+                  <td key={index}>{value}</td>
+                ))}
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Table;
