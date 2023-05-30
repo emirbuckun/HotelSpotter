c7b040a6bd9540e391b104e4c730d0c82f3c204e
Emir Buckun <emirbuckun@hotmail.com>
2023-05-30 06:47:34 +0300

    Import path modified

--- client/src/components/Details/HotelDetails/HotelDetails.jsx
+++ client/src/components/Details/HotelDetails/HotelDetails.jsx
@@ -1,7 +1,7 @@
 import HotelDetailsPhoto from "/src/components/Details/HotelDetails/HotelDetailsPhoto";
 import HotelDetailsInformations from "/src/components/Details/HotelDetails/HotelDetailsInformations";
 import MapComponent from "/src/components/Details/HotelDetails/MapComponent";
-import Comment from "/src/components/CommentComponent";
+import Comment from "/src/components/Details/HotelDetails/CommentComponent";
 
 const HotelDetails = () => {
   return (
