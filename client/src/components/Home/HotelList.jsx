564d307f2d5048c893eaeb8fe1c22687abfc7622
Emir Buckun <emirbuckun@hotmail.com>
2023-05-30 18:49:41 +0300

    Hotel main table data fetched. HotelID, name and rating exist for now

--- client/src/components/Home/HotelList.jsx
+++ client/src/components/Home/HotelList.jsx
@@ -1,58 +1,29 @@
 import React from "react";
+import useFetch from "/src/hooks/useFetch";
 
 const HotelList = () => {
+  const { data } = useFetch(serverURL + "/hotel");
+
   return (
-    <>
-      <div className="container text-center mb-4">
-        <div className="row">
-          <div className="col">
-            <HotelCard />
-          </div>
-          <div className="col">
-            <HotelCard />
-          </div>
-          <div className="col">
-            <HotelCard />
-          </div>
-        </div>
+    <div className="container text-center">
+      <div className="row">
+        {data.length > 0 &&
+          data.map((data, index) => (
+            <div key={index} className="col-lg-4 col-md-6 my-3 d-flex">
+              <HotelCard key={index} data={data} />
+            </div>
+          ))}
       </div>
-
-      <div className="container text-center mb-4">
-        <div className="row">
-          <div className="col">
-            <HotelCard />
-          </div>
-          <div className="col">
-            <HotelCard />
-          </div>
-          <div className="col">
-            <HotelCard />
-          </div>
-        </div>
-      </div>
-
-      <div className="container text-center mb-4">
-        <div className="row">
-          <div className="col">
-            <HotelCard />
-          </div>
-          <div className="col">
-            <HotelCard />
-          </div>
-          <div className="col">
-            <HotelCard />
-          </div>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
 export default HotelList;
 
-const HotelCard = () => {
+const HotelCard = (data) => {
+  const hotel = data.data;
   return (
-    <div className="card" style={{ width: "350px", borderRadius: "15px" }}>
+    <div id={hotel._id} className="card" style={{ borderRadius: "15px" }}>
       <img
         src="https://hotel-su-antalya.hotel-ds.com/data/Imgs/700x500/11087/1108736/1108736295/hotel-su-antalya-img-1.JPEG"
         className="img-fluid"
@@ -65,14 +36,13 @@ const HotelCard = () => {
       />
       <div className="card-body d-flex flex-column justify-content-between">
         <div>
-          <h5 className="card-title mb-0">Malaga, Spain</h5>
-          <p className="card-text">Valentine Hotel</p>
-          <p className="card-text">All inclusive</p>
+          <h5 className="card-title mb-0">Location</h5>
+          <p className="card-text">{hotel.name}</p>
         </div>
         <div className="d-flex align-items-center justify-content-between">
           <div>
             <span className="badge badge-primary"></span>
-            <strong>#{6.8}</strong>
+            <strong>#{hotel.rating}</strong>
           </div>
           <div className="text-end">
             <strong>$100</strong>
