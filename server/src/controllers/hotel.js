1ff968dafdb3b80a82b0be3e2bbf49b2c23a03bc
Emir Buckun <emirbuckun@hotmail.com>
2023-05-31 09:18:17 +0300

    Hotel list fetch method added

--- server/src/controllers/hotel.js
+++ server/src/controllers/hotel.js
@@ -1,4 +1,7 @@
 import { HotelModel } from "../models/Hotel.js";
+import { AmenityModel } from "../models/Amenity.js";
+import { LocationModel } from "../models/Location.js";
+import { PictureModel } from "../models/Picture.js";
 
 export const insertHotel = async (req, res, next) => {
   try {
@@ -36,6 +39,29 @@ export const getHotel = async (req, res, next) => {
   try {
     const hotel = await HotelModel.findById(req.params.id);
     res.status(200).json(hotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getHotelList = async (req, res, next) => {
+  try {
+    var hotelList = [];
+    const hotels = await HotelModel.find();
+
+    for (var i = 0; i < hotels.length; i++) {
+      var hotel = hotels[i].toObject();
+      hotel.amenities = await AmenityModel.find({ hotelID: hotel._id }).select(
+        "amenity -_id"
+      );
+      hotel.location = await LocationModel.findOne({
+        hotelID: hotel._id,
+      }).select("country city -_id");
+      hotel.pictures = await PictureModel.find({ hotelID: hotels[i]._id });
+      hotelList.push(hotel);
+    }
+
+    res.status(200).json(hotelList);
   } catch (err) {
     next(err);
   }
