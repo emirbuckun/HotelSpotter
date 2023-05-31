49cd9b6eb5670b73c26de7296bfaddf9d17c4db9
Emir Buckun <emirbuckun@hotmail.com>
2023-05-31 18:15:01 +0300

    Home component created to lift up the states of search bar and hotel list up

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import HotelList from "./HotelList";
+
+const Home = () => {
+  return (
+    <>
+      <SearchBar />
+      <HotelList />
+    </>
+  );
+};
+
+export default Home;
