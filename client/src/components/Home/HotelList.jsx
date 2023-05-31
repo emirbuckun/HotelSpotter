09e9667e311505bfb62a79b0b194144f5548ecce
Emir Buckun <emirbuckun@hotmail.com>
2023-05-31 12:54:08 +0300

    Hotel listing modified, image and loading added

--- client/src/components/Home/HotelList.jsx
+++ client/src/components/Home/HotelList.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import useFetch from "/src/hooks/useFetch";
 
 const HotelList = () => {
-  const { data } = useFetch(serverURL + "/hotel");
-
+  const { data, loading, error } = useFetch(serverURL + "/hotel/getHotelList");
   return (
     <div className="container text-center">
       <div className="row">
-        {data.length > 0 &&
+        {data.loading ? (
+          <div>Content loading..</div>
+        ) : (
+          data.length > 0 &&
           data.map((data, index) => (
             <div key={index} className="col-lg-4 col-md-6 my-3 d-flex">
               <HotelCard key={index} data={data} />
             </div>
-          ))}
+          ))
+        )}
       </div>
     </div>
   );
@@ -25,23 +28,33 @@ const HotelCard = (data) => {
   return (
     <div id={hotel._id} className="card" style={{ borderRadius: "15px" }}>
       <img
-        src="https://hotel-su-antalya.hotel-ds.com/data/Imgs/700x500/11087/1108736/1108736295/hotel-su-antalya-img-1.JPEG"
-        className="img-fluid"
-        alt="Sampleimage"
+        className="img-fluid card-img-top"
+        alt="Hotel Image"
         style={{
+          minHeight: "15rem",
+          maxHeight: "15rem",
           borderTopLeftRadius: "15px",
           borderTopRightRadius: "15px",
         }}
+        src={
+          hotel.pictures != undefined
+            ? hotel.pictures.picture
+            : "https://hotel-su-antalya.hotel-ds.com/data/Imgs/700x500/11087/1108736/1108736295/hotel-su-antalya-img-1.JPEG"
+        }
       />
       <div className="card-body d-flex flex-column justify-content-between">
         <div>
-          <h5 className="card-title mb-0">Location</h5>
-          <p className="card-text">{hotel.name}</p>
+          <h5 className="card-title mb-0">{hotel.name}</h5>
+          <p className="card-text">
+            {hotel.location.city}, {hotel.location.country}
+          </p>
         </div>
         <div className="d-flex align-items-center justify-content-between">
           <div>
             <span className="badge badge-primary"></span>
-            <strong>#{hotel.rating}</strong>
+            <strong>Rating: {hotel.rating}</strong>
+            <br></br>
+            <strong>Star: {hotel.star}</strong>
           </div>
           <div className="text-end">
             <strong>$100</strong>
