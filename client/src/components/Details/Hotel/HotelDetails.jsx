da5d86407a3d784101e8a51dcf978ceb77f178fd
Emir Buckun <emirbuckun@hotmail.com>
2023-06-01 20:12:14 +0300

    Hotel id sent to each hotel details component

--- client/src/components/Details/Hotel/HotelDetails.jsx
+++ client/src/components/Details/Hotel/HotelDetails.jsx
@@ -2,14 +2,16 @@ import HotelPhoto from "/src/components/Details/Hotel/HotelPhoto";
 import HotelInfo from "/src/components/Details/Hotel/HotelInfo";
 import HotelMap from "/src/components/Details/Hotel/HotelMap";
 import HotelComment from "/src/components/Details/Hotel/HotelComment";
+import { useParams } from "react-router-dom";
 
 const HotelDetails = () => {
+  const { id } = useParams();
   return (
     <>
-      <HotelPhoto />
-      <HotelInfo />
-      <HotelMap />
-      <HotelComment />
+      <HotelPhoto id={id} />
+      <HotelInfo id={id} />
+      <HotelMap id={id} />
+      <HotelComment id={id} />
     </>
   );
 };
