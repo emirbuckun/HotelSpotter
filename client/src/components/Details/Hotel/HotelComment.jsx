168b579615b0503c940292ea549c35968500caad
Emir Buckun <emirbuckun@hotmail.com>
2023-06-02 12:21:22 +0300

    Comment style modified, fetch operation added

--- client/src/components/Details/Hotel/HotelComment.jsx
+++ client/src/components/Details/Hotel/HotelComment.jsx
@@ -1,8 +1,5 @@
-import React from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Typography from "@mui/material/Typography";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { styled } from "@mui/material/styles";
 import Rating from "@mui/material/Rating";
 import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
@@ -20,27 +17,27 @@ const StyledRating = styled(Rating)(({ theme }) => ({
 const customIcons = {
   1: {
     icon: (
-      <SentimentVeryDissatisfiedIcon color="error" style={{ fontSize: 40 }} />
+      <SentimentVeryDissatisfiedIcon color="error" style={{ fontSize: 30 }} />
     ),
     label: "Very Dissatisfied",
   },
   2: {
-    icon: <SentimentDissatisfiedIcon color="error" style={{ fontSize: 40 }} />,
+    icon: <SentimentDissatisfiedIcon color="error" style={{ fontSize: 30 }} />,
     label: "Dissatisfied",
   },
   3: {
-    icon: <SentimentSatisfiedIcon color="warning" style={{ fontSize: 40 }} />,
+    icon: <SentimentSatisfiedIcon color="warning" style={{ fontSize: 30 }} />,
     label: "Neutral",
   },
   4: {
     icon: (
-      <SentimentSatisfiedAltIcon color="success" style={{ fontSize: 40 }} />
+      <SentimentSatisfiedAltIcon color="success" style={{ fontSize: 30 }} />
     ),
     label: "Satisfied",
   },
   5: {
     icon: (
-      <SentimentVerySatisfiedIcon color="success" style={{ fontSize: 40 }} />
+      <SentimentVerySatisfiedIcon color="success" style={{ fontSize: 30 }} />
     ),
     label: "Very Satisfied",
   },
@@ -51,108 +48,60 @@ function IconContainer(props) {
   return <span {...other}>{customIcons[value].icon}</span>;
 }
 
-const HotelComment = () => {
+const HotelComment = (hotelData) => {
+  const reviews = hotelData.hotelData.review;
   return (
     <div className="container">
-      <div className="row justify-content-center">
-        <div className="col">
-          <List
-            id="comment-list"
-            subheader={<Typography variant="h5">Comments</Typography>}
-            sx={{
-              width: "100%",
-              bgcolor: "background.paper",
-            }}
-          >
-            <ListItem
-              id="comment-list-item"
-              alignItems="flex-start"
-              divider={true}
-            >
-              <ListItemText
-                primary={
-                  <>
-                    <Typography id="name" variant="h6">
-                      John Doe
-                    </Typography>
-                    <Typography id="date">2021-10-10</Typography>
-                  </>
-                }
-                secondary={
-                  <>
-                    <div>
-                      <StyledRating
-                        id="rating"
-                        name="highlight-selected-only"
-                        defaultValue={2}
-                        IconContainerComponent={IconContainer}
-                        getLabelText={(value) => customIcons[value].label}
-                        highlightSelectedOnly
-                        readOnly
-                      />
-                    </div>
-                    <div>
-                      <Typography
-                        id="comment"
-                        sx={{ display: "inline" }}
-                        component="span"
-                        color="text.primary"
-                      >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Integer posuere erat a ante.
-                      </Typography>
-                    </div>
-                  </>
-                }
-              />
-            </ListItem>
-            <ListItem
-              id="comment-list-item"
-              alignItems="flex-start"
-              divider={true}
-            >
-              <ListItemText
-                primary={
-                  <>
-                    <Typography id="name" variant="h6">
-                      John Doe
-                    </Typography>
-                    <Typography id="date">2021-10-10</Typography>
-                  </>
-                }
-                secondary={
-                  <>
-                    <div>
-                      <StyledRating
-                        id="rating"
-                        name="highlight-selected-only"
-                        defaultValue={4}
-                        IconContainerComponent={IconContainer}
-                        getLabelText={(value) => customIcons[value].label}
-                        highlightSelectedOnly
-                        readOnly
-                      />
-                    </div>
-                    <div>
-                      <Typography
-                        id="comment"
-                        sx={{ display: "inline" }}
-                        component="span"
-                        color="text.primary"
-                      >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Integer posuere erat a ante.
-                      </Typography>
-                    </div>
-                  </>
-                }
-              />
-            </ListItem>
-          </List>
-        </div>
-      </div>
+      <h3>Comments</h3>
+      {reviews.length > 0 ? (
+        reviews.map((data, index) => <Comment key={index} data={data} />)
+      ) : (
+        <div>There is no any comment for this hotel.</div>
+      )}
     </div>
   );
 };
 
 export default HotelComment;
+
+const Comment = (data) => {
+  const [userName, setUserName] = useState("");
+  const review = data.data;
+
+  const getUserDetails = async (userID) => {
+    try {
+      const response = await axios.get(serverURL + "/user/" + userID);
+      setUserName(response.data.firstName + " " + response.data.lastName);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getUserDetails(review.userID);
+  }, []);
+
+  return (
+    <div className="list-group">
+      <a className="list-group-item list-group-item-action flex-column align-items-start">
+        <div className="d-flex w-100 justify-content-between">
+          <h5 className="mb-1">{userName}</h5>
+          <small className="text-muted">
+            {new Date(review.createDate).toLocaleDateString("tr-TR", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+            })}
+          </small>
+        </div>
+        <p className="mb-1">{review.description}</p>
+        <StyledRating
+          value={review.rating}
+          IconContainerComponent={IconContainer}
+          highlightSelectedOnly
+          readOnly
+        />
+      </a>
+    </div>
+  );
+};
