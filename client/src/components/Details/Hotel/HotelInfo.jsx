d863105b56905ad80668f1fac0baafb140d69d3e
Emir Buckun <emirbuckun@hotmail.com>
2023-06-02 14:15:37 +0300

    Hotel details shown in the info component

--- client/src/components/Details/Hotel/HotelInfo.jsx
+++ client/src/components/Details/Hotel/HotelInfo.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import TextField from "@mui/material/TextField";
 
 const HotelInfo = (hotelData) => {
-  const [guests, setGuests] = React.useState(1);
-  const [price, setPrice] = React.useState(700);
-  const fixedPrice = 700;
+  const [guests, setGuests] = useState(1);
+  const [price, setPrice] = useState();
+  const data = hotelData.hotelData;
+  const amenities = data.amenities ? data.amenities.amenity : [];
+  const rooms = data.room ? data.room : [];
+  const minPrice =
+    rooms.length > 0 ? rooms.find((x) => x.roomType === "Single").price : 1000;
+  const fixedPrice = minPrice != null ? minPrice : 1000;
+  const location = data.location;
+  const locationText =
+    location.street +
+    " St. No: " +
+    location.number +
+    " Zip: " +
+    location.zip +
+    " " +
+    location.city +
+    "/" +
+    location.country;
+
+  useEffect(() => {
+    setPrice(minPrice);
+  }, []);
 
   const handleGuestsDecrease = () => {
     var price = fixedPrice * (guests - 1);
     if (guests > 1) {
       setGuests(guests - 1);
       setPrice(price);
-    }
-    if (guests === 1) {
-      setPrice(fixedPrice);
     }
   };
 
@@ -25,62 +42,92 @@ const HotelInfo = (hotelData) => {
     setGuests(guests + 1);
     setPrice(price);
   };
-  const data = hotelData.hotelData;
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-8">
-          There is a gym and fitness center. This hotel offers suite rooms to
-          its customers. 3 meals are served. Customers have the opportunity to
-          eat unlimitedly. Indoor and outdoor swimming pool available This hotel
-          is by the sea. Hotel is located in Malaga and the most important
-          feature of this city is.. There is a gym and fitness center. This
-          hotel offers suite rooms to its customers. 3 meals are served.
-          Customers have the opportunity to eat unlimitedly. Indoor and outdoor
-          swimming pool available This hotel is by the sea. Hotel is located in
-          Malaga and the most important feature of this city is..
+          <div className="row">
+            <div className="col-7">
+              <h5 className="text-center">Details</h5>
+              <div className="card card-body">
+                <div>
+                  <strong>Name:</strong> {data.name}
+                </div>
+                <div>
+                  <strong>Star:</strong> {data.star}
+                </div>
+                <div>
+                  <strong>Rating:</strong> {data.rating}
+                </div>
+                <div>
+                  <strong>Location:</strong> {locationText}
+                </div>
+                <div>
+                  <strong>Rooms:</strong>
+                  {rooms.map((data, index) => (
+                    <>
+                      <div key={index}>
+                        {data.roomType}: {data.price}$
+                      </div>
+                    </>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="col-5">
+              <h5 className="text-center">Amenities</h5>
+              <div className="card card-body">
+                <ul className="list-group list-group-flush">
+                  {amenities.length > 0 ? (
+                    amenities.map((data, index) => (
+                      <li key={index} className="list-group-item">
+                        <strong>{index + 1} -</strong> {data}
+                      </li>
+                    ))
+                  ) : (
+                    <div>There is no any specified amenity in this hotel.</div>
+                  )}
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-4">
-          <div className="card">
-            <div className="card-body">
-              <div className="d-flex justify-content-center align-items-center">
-                <h5 className="card-title">{price} USD per night</h5>
-              </div>
-
-              <div className="d-flex justify-content-center align-items-center">
-                <Button
-                  variant="contained"
-                  color="error"
-                  sx={{
-                    height: "60px",
-                    width: "60px",
-                    margin: "10px",
-                  }}
-                  onClick={handleGuestsDecrease}
-                >
-                  <RemoveIcon />
-                </Button>
-                <TextField
-                  id="outlined-number"
-                  label="Guests"
-                  variant="outlined"
-                  value={guests}
-                  sx={{ width: "100px", margin: "10px" }}
-                />
-                <Button
-                  variant="contained"
-                  color="success"
-                  sx={{ height: "60px", width: "60px", margin: "10px" }}
-                  onClick={handleGuestsIncrease}
-                >
-                  <AddIcon />
-                </Button>
-              </div>
-              <div className="d-flex justify-content-center align-items-center">
-                <a href="#" className="btn btn-danger">
-                  Reserve
-                </a>
-              </div>
+          <h5 className="text-center">Reserve</h5>
+          <div className="card card-body">
+            <div className="d-flex justify-content-center align-items-center">
+              <h5 className="card-title">{price} USD night</h5>
+            </div>
+            <div className="d-flex justify-content-center align-items-center">
+              <Button
+                variant="contained"
+                color="error"
+                sx={{
+                  height: "60px",
+                }}
+                onClick={handleGuestsDecrease}
+              >
+                <RemoveIcon />
+              </Button>
+              <TextField
+                id="outlined-number"
+                label="Guests"
+                variant="outlined"
+                value={guests}
+                sx={{ width: "100px", margin: "10px" }}
+              />
+              <Button
+                variant="contained"
+                color="success"
+                sx={{ height: "60px" }}
+                onClick={handleGuestsIncrease}
+              >
+                <AddIcon />
+              </Button>
+            </div>
+            <div className="d-flex justify-content-center align-items-center">
+              <a className="btn btn-primary">Reserve</a>
             </div>
           </div>
         </div>
