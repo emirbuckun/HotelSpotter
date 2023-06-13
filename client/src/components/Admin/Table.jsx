a2e99d3dade428dc732668910afb48d29e33ee39
Emir Buckun <emirbuckun@hotmail.com>
2023-06-13 11:47:31 +0300

    Amenity data parsing operation added

--- client/src/components/Admin/Table.jsx
+++ client/src/components/Admin/Table.jsx
@@ -1,35 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useFetch from "/src/hooks/useFetch";
 
 const Table = (name) => {
   const pageName = name.name;
-  const { data, loading, error } = useFetch(serverURL + "/" + pageName);
+  const [table, setTable] = useState([]);
+  const { data } = useFetch(serverURL + "/" + pageName);
 
-  data.length > 0 &&
-    data.forEach(function (obj) {
-      delete obj.__v;
-      pageName == "user" && delete obj.password;
-    });
+  useEffect(() => {
+    if (data.length > 0) {
+      clearData();
+      parseData();
+      setTable(data);
+    }
+  }, [data]);
+
+  // Clear "__v" column in every table
+  // Clear "password" column if table is user table
+  const clearData = () => {
+    data.length > 0 &&
+      data.forEach(function (obj) {
+        delete obj.__v;
+        pageName == "user" && delete obj.password;
+      });
+  };
+
+  const parseData = () => {
+    console.log(pageName);
+    switch (pageName) {
+      case "amenity":
+        parseAmenityData();
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const parseAmenityData = () => {
+    console.log("parseAmenityData");
+    data.length > 0 &&
+      data.forEach(function (obj) {
+        obj.amenity = obj.amenity.join(" - ");
+      });
+  };
 
   return (
     <>
-      <div className="table"></div>
       <table className="table table-striped table-hover">
         <thead>
           <tr>
-            {data.length > 0 &&
-              Object.keys(data[0]).map((key, index) => (
-                <th key={index}>{key}</th>
-              ))}
+            {table.length > 0 &&
+              Object.keys(table[0]).map(
+                (key, index) => key != "_id" && <th key={index}>{key}</th>
+              )}
           </tr>
         </thead>
         <tbody>
-          {data.length > 0 &&
-            data.map((data, index) => (
+          {table.length > 0 &&
+            table.map((table, index) => (
               <tr key={index}>
-                {Object.values(data).map((value, index) => (
-                  <td key={index}>{value}</td>
-                ))}
+                {Object.values(table).map(
+                  (value, index) => index != 0 && <td key={index}>{value}</td>
+                )}
               </tr>
             ))}
         </tbody>
