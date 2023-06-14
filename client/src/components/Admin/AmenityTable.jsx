6c6b145fedb192e595862802575527af0fa387f5
Emir Buckun <emirbuckun@hotmail.com>
2023-06-14 07:58:39 +0300

    Loading and buttons added

--- client/src/components/Admin/AmenityTable.jsx
+++ client/src/components/Admin/AmenityTable.jsx
@@ -3,7 +3,7 @@ import useFetch from "/src/hooks/useFetch";
 
 const AmenityTable = (name) => {
   const [table, setTable] = useState([]);
-  const { data } = useFetch(serverURL + "/amenity");
+  const { data, loading } = useFetch(serverURL + "/amenity");
 
   useEffect(() => {
     if (data.length > 0) {
@@ -15,31 +15,51 @@ const AmenityTable = (name) => {
   const parseAmenityData = () => {
     data.length > 0 &&
       data.forEach(function (obj) {
-        obj.amenity = obj.amenity.join(" - ");
+        obj.amenity = obj.amenity.join(", ");
       });
   };
 
   return (
-    <table className="table table-striped table-hover">
-      <thead>
-        <tr>
-          {table.length > 0 &&
-            Object.keys(table[0]).map(
-              (key, index) => index > 1 && <th key={index}>{key}</th>
-            )}
-        </tr>
-      </thead>
-      <tbody>
-        {table.length > 0 &&
-          table.map((table, index) => (
-            <tr key={index}>
-              {Object.values(table).map(
-                (value, index) => index > 1 && <td key={index}>{value}</td>
-              )}
-            </tr>
-          ))}
-      </tbody>
-    </table>
+    <>
+      {loading || table.length <= 0 ? (
+        <div className="text-center">Loading</div>
+      ) : (
+        <>
+          <button type="button" className="btn btn-success float-end">
+            Create New
+          </button>
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th>Amenities</th>
+                <th>Hotel Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {table.length > 0 &&
+                table.map((table, index) => (
+                  <tr key={index}>
+                    {Object.values(table).map(
+                      (value, index) =>
+                        index > 1 && <td key={index}>{value}</td>
+                    )}
+                    <td>
+                      <button type="button" className="btn btn-primary">
+                        Edit
+                      </button>
+                    </td>
+                    <td>
+                      <button type="button" className="btn btn-danger">
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </>
+      )}
+    </>
   );
 };
 
