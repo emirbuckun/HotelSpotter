939ace051c6be6ca201dfee673948c6d30eea914
Emir Buckun <emirbuckun@hotmail.com>
2023-07-06 10:03:16 +0300

    Sidebar active link bug fixed

--- client/src/components/Admin/Sidebar.jsx
+++ client/src/components/Admin/Sidebar.jsx
@@ -7,80 +7,79 @@ const Sidebar = () => {
         <div className="list-group list-group-flush mx-3 mt-4">
           <a
             href="/admin/amenity"
-            className="list-group-item list-group-item-action py-2 ripple active"
-            aria-current="true"
+            className="list-group-item list-group-item-action py-2 ripple"
           >
             Amenity
           </a>
           <a
             href="/admin/answer"
-            className="list-group-item list-group-item-action py-2 ripple "
+            className="list-group-item list-group-item-action py-2 ripple"
           >
             Answer
           </a>
           <a
             href="/admin/hotel"
-            className="list-group-item list-group-item-action py-2 "
+            className="list-group-item list-group-item-action py-2"
           >
             Hotel
           </a>
           <a
             href="/admin/location"
-            className="list-group-item list-group-item-action py-2 "
+            className="list-group-item list-group-item-action py-2"
           >
             Location
           </a>
           <a
             href="/admin/log"
-            className="list-group-item list-group-item-action py-2 "
+            className="list-group-item list-group-item-action py-2"
           >
             Log
           </a>
           <a
             href="/admin/ownership"
-            className="list-group-item list-group-item-action py-2 "
+            className="list-group-item list-group-item-action py-2"
           >
             Ownership
           </a>
           <a
             href="/admin/picture"
-            className="list-group-item list-group-item-action py-2 "
+            className="list-group-item list-group-item-action py-2"
           >
             Picture
           </a>
           <a
             href="/admin/question"
-            className="list-group-item list-group-item-action py-2 "
+            className="list-group-item list-group-item-action py-2"
           >
             Question
           </a>
           <a
             href="/admin/reservation"
-            className="list-group-item list-group-item-action py-2 "
+            className="list-group-item list-group-item-action py-2"
           >
             Reservation
           </a>
           <a
             href="/admin/review"
-            className="list-group-item list-group-item-action py-2 "
+            className="list-group-item list-group-item-action py-2"
           >
             Review
           </a>
           <a
             href="/admin/room"
-            className="list-group-item list-group-item-action py-2 "
+            className="list-group-item list-group-item-action py-2"
           >
             Room
           </a>
           <a
             href="/admin/user"
-            className="list-group-item list-group-item-action py-2 "
+            className="list-group-item list-group-item-action py-2"
           >
             User
           </a>
           <a
             href="/admin/userrole"
-            className="list-group-item list-group-item-action py-2 "
+            className="list-group-item list-group-item-action py-2"
           >
             User Role
           </a>
