c495f649989c998b4eb9375a8246650e668b9a57
Emir Buckun <emirbuckun@hotmail.com>
2023-07-10 18:07:20 +0300

    Review get method modified

--- server/src/controllers/review.js
+++ server/src/controllers/review.js
@@ -52,8 +52,45 @@ export const getByReservationID = async (req, res, next) => {
 
 export const getReviews = async (req, res, next) => {
   try {
-    const reviews = await ReviewModel.find();
-    res.status(200).json(reviews);
+    const { page, limit, sort } = req.query;
+
+    const sortArgs = sort ? sort.split(",") : ["_id", "asc"];
+    const sortField = sortArgs[0];
+    const sortOrder = sortArgs[1] == "desc" ? -1 : 1;
+
+    const query = await ReviewModel.aggregate()
+      .lookup({
+        from: "users",
+        localField: "userID",
+        foreignField: "_id",
+        as: "user",
+      })
+      .lookup({
+        from: "hotels",
+        localField: "hotelID",
+        foreignField: "_id",
+        as: "hotel",
+      })
+      .lookup({
+        from: "reservations",
+        localField: "reservationID",
+        foreignField: "_id",
+        as: "reservation",
+      })
+      .unwind("user")
+      .unwind("hotel")
+      .unwind("reservation")
+      .sort({ [sortField]: parseInt(sortOrder) })
+      .facet({
+        count: [{ $count: "total" }],
+        paginated: [{ $skip: page * limit }, { $limit: parseInt(limit) }],
+      });
+
+    const result = query[0];
+    const list = result.paginated;
+    const total = result.count[0] != null ? result.count[0].total : 0;
+
+    res.status(200).json({ total, list });
   } catch (err) {
     next(err);
   }
