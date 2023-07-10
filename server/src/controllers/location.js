77ed54740c65b5ef1634180313f59f8c7898d4e2
Emir Buckun <emirbuckun@hotmail.com>
2023-07-10 17:54:39 +0300

    Location get method modified

--- server/src/controllers/location.js
+++ server/src/controllers/location.js
@@ -54,8 +54,31 @@ export const getLocationByHotelID = async (req, res, next) => {
 
 export const getLocations = async (req, res, next) => {
   try {
-    const locations = await LocationModel.find();
-    res.status(200).json(locations);
+    const { page, limit, sort } = req.query;
+
+    const sortArgs = sort ? sort.split(",") : ["_id", "asc"];
+    const sortField = sortArgs[0];
+    const sortOrder = sortArgs[1] == "desc" ? -1 : 1;
+
+    const query = await LocationModel.aggregate()
+      .lookup({
+        from: "hotels",
+        localField: "hotelID",
+        foreignField: "_id",
+        as: "hotel",
+      })
+      .unwind("hotel")
+      .sort({ [sortField]: parseInt(sortOrder) })
+      .facet({
+        count: [{ $count: "total" }],
+        paginated: [{ $skip: page * limit }, { $limit: parseInt(limit) }],
+      });
+
+    const result = query[0];
+    const list = result.paginated;
+    const total = result.count[0] != null ? result.count[0].total : 0;
+
+    res.status(200).json({ total, list });
   } catch (err) {
     next(err);
   }
