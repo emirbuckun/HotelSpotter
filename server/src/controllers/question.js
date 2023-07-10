830bbb08a60e82e52028ce0d49833ee307c84f85
Emir Buckun <emirbuckun@hotmail.com>
2023-07-10 18:07:06 +0300

    Question get method modified

--- server/src/controllers/question.js
+++ server/src/controllers/question.js
@@ -45,8 +45,38 @@ export const getQuestion = async (req, res, next) => {
 
 export const getQuestions = async (req, res, next) => {
   try {
-    const questions = await QuestionModel.find();
-    res.status(200).json(questions);
+    const { page, limit, sort } = req.query;
+
+    const sortArgs = sort ? sort.split(",") : ["_id", "asc"];
+    const sortField = sortArgs[0];
+    const sortOrder = sortArgs[1] == "desc" ? -1 : 1;
+
+    const query = await QuestionModel.aggregate()
+      .lookup({
+        from: "users",
+        localField: "userID",
+        foreignField: "_id",
+        as: "user",
+      })
+      .lookup({
+        from: "hotels",
+        localField: "hotelID",
+        foreignField: "_id",
+        as: "hotel",
+      })
+      .unwind("user")
+      .unwind("hotel")
+      .sort({ [sortField]: parseInt(sortOrder) })
+      .facet({
+        count: [{ $count: "total" }],
+        paginated: [{ $skip: page * limit }, { $limit: parseInt(limit) }],
+      });
+
+    const result = query[0];
+    const list = result.paginated;
+    const total = result.count[0] != null ? result.count[0].total : 0;
+
+    res.status(200).json({ total, list });
   } catch (err) {
     next(err);
   }
