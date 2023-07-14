141e69cd53b1d5683ff296f38306addf8d662ccb
Emir Büçkün <emirbuckun@hotmail.com>
2023-07-14 17:38:42 +0300

    Comment Removed From PaginationItem.jsx

--- client/src/components/Admin/PaginationItem.jsx
+++ client/src/components/Admin/PaginationItem.jsx
@@ -19,25 +19,3 @@ const PaginationItem = ({ page, total, limit, setPage }) => {
 };
 
 export default PaginationItem;
-
-/*const Pagination = ({ page, total, limit, setPage }) => {
-  const totalPages = Math.ceil(total / limit);
-  return (
-    <BootstrapPagination>
-      <BootstrapPagination.Prev
-        disabled={page == 0}
-        onClick={() => setPage(page - 1)}
-      />
-      {totalPages > 0 &&
-        [...Array(totalPages)].map((_, index) => (
-          <BootstrapPagination.Item key={index} onClick={() => setPage(index)}>
-            {index + 1}
-          </BootstrapPagination.Item>
-        ))}
-      <BootstrapPagination.Next
-        disabled={page == totalPages - 1}
-        onClick={() => setPage(page + 1)}
-      />
-    </BootstrapPagination>
-  );
-};*/
